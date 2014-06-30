ae3d3e943d66433644b901834f05b69e4caa5c3f
mkerr <mkerr@openboxsoftware.com>
2014-06-30 17:53:47 +0200

    Added fragments to location activity

--- src/com/example/icreep/LocationActivity.java
+++ src/com/example/icreep/LocationActivity.java
@@ -1,163 +1,145 @@
 package com.example.icreep;
 
-import java.util.ArrayList;
-import java.util.Locale;
-
+import android.app.ActionBar.Tab;
+import android.app.ActionBar.TabListener;
 import android.app.Activity;
 import android.app.ActionBar;
-import android.app.Fragment;
-import android.app.FragmentManager;
+import android.support.v4.app.Fragment;
 import android.app.FragmentTransaction;
-import android.support.v13.app.FragmentPagerAdapter;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
-import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
+import android.os.Build;
 
-public class LocationActivity extends FragmentActivity {
-	
-	int NUMSCREENS = 2;
+public class LocationActivity extends FragmentActivity implements TabListener {
 
-	/**
-	 * The {@link android.support.v4.view.PagerAdapter} that will provide
-	 * fragments for each of the sections. We use a {@link FragmentPagerAdapter}
-	 * derivative, which will keep every loaded fragment in memory. If this
-	 * becomes too memory intensive, it may be best to switch to a
-	 * {@link android.support.v13.app.FragmentStatePagerAdapter}.
-	 */
-	SectionsPagerAdapter mSectionsPagerAdapter;
-
-	/**
-	 * The {@link ViewPager} that will host the section contents.
-	 */
-	ViewPager mViewPager;
+	ActionBar actionBar;
+	ViewPager viewPager;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_location);
 
-		// Create the adapter that will return a fragment for each of the two
-		// primary sections of the activity.
-		mSectionsPagerAdapter = new SectionsPagerAdapter(getFragmentManager());
+		viewPager = (ViewPager) findViewById(R.id.location_pager);
+		viewPager.setAdapter(new MyAdapter(getSupportFragmentManager()));
 
-		// Set up the ViewPager with the sections adapter.
-		mViewPager = (ViewPager) findViewById(R.id.pager);
-		mViewPager.setAdapter(mSectionsPagerAdapter);
+		viewPager.setOnPageChangeListener(new ViewPager.OnPageChangeListener() {
 
-	}
-
-
-	/**
-	 * A {@link FragmentPagerAdapter} that returns a fragment corresponding to
-	 * one of the sections/tabs/pages.
-	 */
-	public class SectionsPagerAdapter extends FragmentPagerAdapter {
-
-		public SectionsPagerAdapter(FragmentManager fm) {
-			super(fm);
-		}
-
-		@Override
-		public Fragment getItem(int position) {
-			// getItem is called to instantiate the fragment for the given page.
-			// Return a PlaceholderFragment (defined as a static inner class
-			// below).
-			return PlaceholderFragment.newInstance(position + 1);
-		}
-
-		@Override
-		public int getCount() {
-			// Show 2 total pages.
-			return NUMSCREENS;
-		}
-
-		@Override
-		public CharSequence getPageTitle(int position) {
-			Locale l = Locale.getDefault();
-			switch (position) {
-			case 0:
-				return getString(R.string.title_location_section1).toUpperCase(l);
-			case 1:
-				return getString(R.string.title_location_section2).toUpperCase(l);
+			@Override
+			public void onPageSelected(int arg0) {
+				actionBar.setSelectedNavigationItem(arg0);
 			}
-			return null;
-		}
+
+			@Override
+			public void onPageScrolled(int arg0, float arg1, int arg2) {
+			}
+
+			@Override
+			public void onPageScrollStateChanged(int arg0) {
+			}
+		});
+
+		actionBar = getActionBar();
+		actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
+
+		ActionBar.Tab tab1 = actionBar.newTab();
+		tab1.setText("Current Location");
+		tab1.setTabListener(this);
+
+		ActionBar.Tab tab2 = actionBar.newTab();
+		tab2.setText("Visited Zones");
+		tab2.setTabListener(this);
+
+		actionBar.addTab(tab1);
+		actionBar.addTab(tab2);
 	}
 
-	/**
-	 * A placeholder fragment containing a simple view.
-	 */
-	public static class PlaceholderFragment extends Fragment {
-		/**
-		 * The fragment argument representing the section number for this
-		 * fragment.
-		 */
-		private static final String ARG_SECTION_NUMBER = "section_number";
-
-		/**
-		 * Returns a new instance of this fragment for the given section number.
-		 */
-		public static PlaceholderFragment newInstance(int sectionNumber) {
-			PlaceholderFragment fragment = new PlaceholderFragment();
-			Bundle args = new Bundle();
-			args.putInt(ARG_SECTION_NUMBER, sectionNumber);
-			fragment.setArguments(args);
-			return fragment;
-		}
-
-		public PlaceholderFragment() {
-		}
-
-		@Override
-		public View onCreateView(LayoutInflater inflater, ViewGroup container,
-				Bundle savedInstanceState) {
-			View rootView = inflater.inflate(R.layout.fragment_location,
-					container, false);
-			TextView textView = (TextView) rootView
-					.findViewById(R.id.section_label);
-			textView.setText(Integer.toString(getArguments().getInt(
-					ARG_SECTION_NUMBER)));
-			return rootView;
-		}
-	}
-	
 	/**
 	 * Queries the Location object to find which zone the user is currently in
-	 * @param location - the location object
-	 * @return zone - the current zone 
+	 * 
+	 * @param location
+	 *            - the location object
+	 * @return zone - the current zone
 	 */
-	private int getCurrentZone(UserLocation location){
-		
+	private int getCurrentZone(UserLocation location) {
+
 	}
-	
+
 	/**
 	 * Updates the displayed map with the current location of the user
 	 */
-	private void updateMap(){
-		
-	}
-	
-	/**
-	 * Queries the Location object to determine where the user has been that day
-	 * @param location - the location object
-	 * @return zones - an array representing all of the zones the user has visited that day
-	 */
-	private ArrayList<Integer> findDailyMovements(UserLocation location){
-		
-	}
-	
-	/**
-	 * Updates the table that displays all of the zones the user has been that day
-	 */
-	private void updateDailyTable(){
-		
+	private void updateMap() {
+
 	}
 
+	/**
+	 * Queries the Location object to determine where the user has been that day
+	 * 
+	 * @param location
+	 *            - the location object
+	 * @return zones - an array representing all of the zones the user has
+	 *         visited that day
+	 */
+	private ArrayList<Integer> findDailyMovements(UserLocation location) {
+
+	}
+
+	/**
+	 * Updates the table that displays all of the zones the user has been that
+	 * day
+	 */
+	private void updateDailyTable() {
+
+	}
+
+	@Override
+	public void onTabSelected(Tab tab, FragmentTransaction ft) {
+		viewPager.setCurrentItem(tab.getPosition());
+	}
+
+	@Override
+	public void onTabUnselected(Tab tab, FragmentTransaction ft) {
+	}
+
+	@Override
+	public void onTabReselected(Tab tab, FragmentTransaction ft) {
+	}
+}
+
+class MyAdapter extends FragmentPagerAdapter {
+
+	private final int NUMSCREENS = 2;
+
+	public MyAdapter(FragmentManager fm) {
+		super(fm);
+
+	}
+
+	@Override
+	public Fragment getItem(int arg0) {
+		Fragment fragment = null;
+
+		if (arg0 == 0) {
+			fragment = new LocationFragmentA();
+		}
+		if (arg0 == 1) {
+			fragment = new LocationFragmentB();
+		}
+
+		return fragment;
+	}
+
+	@Override
+	public int getCount() {
+		// TODO Auto-generated method stub
+		return NUMSCREENS;
+	}
 }
--- src/com/example/icreep/reportActivity.java
+++ src/com/example/icreep/reportActivity.java
@@ -1,7 +1,5 @@
 package com.example.icreep;
 
-
-
 import android.app.Activity;
 import android.os.Bundle;
 import android.util.Log;
@@ -10,89 +8,81 @@ import android.view.MenuItem;
 
 public class reportActivity extends Activity {
 
-	 @Override
-	    protected void onCreate(Bundle savedInstanceState) {
-	        super.onCreate(savedInstanceState);
-	        setContentView(R.layout.reports);    
-	       
-	    }
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.reports);
 
+	}
 
-	    @Override
-	    public boolean onCreateOptionsMenu(Menu menu) {
-	        
-	        // Inflate the menu; this adds items to the action bar if it is present.
-	        getMenuInflater().inflate(R.menu.main, menu);
-	        return true;
-	    }
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
 
-	    @Override
-	    public boolean onOptionsItemSelected(MenuItem item) {
-	        // Handle action bar item clicks here. The action bar will
-	        // automatically handle clicks on the Home/Up button, so long
-	        // as you specify a parent activity in AndroidManifest.xml.
-	        int id = item.getItemId();
-	        if (id == R.id.action_settings) {
-	            return true;
-	        }
-	        return super.onOptionsItemSelected(item);
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
 
-	    }
-	    
-	    @Override
-	    protected void onStart() 
-	    {
-	        super.onStart();
-	        Log.d("onStart", "onStart");
-	    }    
-	    
-	    @Override
-	    protected void onResume() 
-	    {
-	        super.onResume();
-	        Log.d("onResume", "onResume");  
-	    }    
-	      
-	    @Override
-	    protected void onPause() 
-	    {
-	        super.onPause();
-	        Log.d("onPause", "onPause");
-	    }    
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
 
-	    @Override
-	    protected void onStop() 
-	    {
-	        super.onStop();
-	        Log.d("vince", "onStop");
-	    }    
-	    
-	    @Override
-	    protected void onRestart() 
-	    {
-	        super.onRestart();
-	        Log.d("onRestart", "onRestart");
-	    }    
-	    
-	    @Override
-	    protected void onDestroy() 
-	    {
-	    	super.onDestroy();
-	    	Log.d("vince", "onDestroy");
-	    }
-	    
-	   @Override
-	   protected void onSaveInstanceState(Bundle out) 
-	    {
-	    	super.onSaveInstanceState(out);
-	    	
-	    }
-	    
-	    @Override
-	    protected void onRestoreInstanceState(Bundle savedInstanceState) {
-	    	// TODO Auto-generated method stub
-	    	super.onRestoreInstanceState(savedInstanceState);
-	    	
-	    }
-	
+	}
+
+	@Override
+	protected void onStart() {
+		super.onStart();
+		Log.d("onStart", "onStart");
+	}
+
+	@Override
+	protected void onResume() {
+		super.onResume();
+		Log.d("onResume", "onResume");
+	}
+
+	@Override
+	protected void onPause() {
+		super.onPause();
+		Log.d("onPause", "onPause");
+	}
+
+	@Override
+	protected void onStop() {
+		super.onStop();
+		Log.d("vince", "onStop");
+	}
+
+	@Override
+	protected void onRestart() {
+		super.onRestart();
+		Log.d("onRestart", "onRestart");
+	}
+
+	@Override
+	protected void onDestroy() {
+		super.onDestroy();
+		Log.d("vince", "onDestroy");
+	}
+
+	@Override
+	protected void onSaveInstanceState(Bundle out) {
+		super.onSaveInstanceState(out);
+
+	}
+
+	@Override
+	protected void onRestoreInstanceState(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onRestoreInstanceState(savedInstanceState);
+
+	}
+
 }
