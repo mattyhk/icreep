54a93858d17200e8378622914d26aeb7dd725f45
ncharlton <ncharlton@openboxsoftware.com>
2014-06-30 18:01:27 +0200

    1. Created profile creation activity/screen.
    2. Created iCreep Main Menu activity/screen.
    3. Created button_stlye XML file in drawables.

--- gen/com/example/icreep/R.java
+++ gen/com/example/icreep/R.java
@@ -10,6 +10,9 @@ package com.example.icreep;
 public final class R {
     public static final class attr {
     }
+    public static final class color {
+        public static final int background=0x7f060000;
+    }
     public static final class dimen {
         /**  Default screen margins, per the Android Design guidelines. 
 
@@ -22,22 +25,61 @@ public final class R {
         public static final int activity_vertical_margin=0x7f040001;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int button_style=0x7f020000;
+        public static final int ic_launcher=0x7f020001;
     }
     public static final class id {
-        public static final int action_settings=0x7f080001;
-        public static final int container=0x7f080000;
+        public static final int action_settings=0x7f09000d;
+        public static final int button1=0x7f090002;
+        public static final int button2=0x7f090003;
+        public static final int button3=0x7f090004;
+        public static final int button4=0x7f090005;
+        public static final int container=0x7f090000;
+        public static final int editText1=0x7f090006;
+        public static final int editText2=0x7f090008;
+        public static final int editText3=0x7f09000a;
+        public static final int imageView1=0x7f09000c;
+        public static final int textView1=0x7f090001;
+        public static final int textView2=0x7f090007;
+        public static final int textView3=0x7f090009;
+        public static final int textView4=0x7f09000b;
     }
     public static final class layout {
-        public static final int activity_main=0x7f030000;
+        public static final int activity_icreep_menu=0x7f030000;
+        public static final int activity_main=0x7f030001;
+        public static final int activity_profile_creation=0x7f030002;
+        public static final int fragment_icreep_menu=0x7f030003;
+        public static final int fragment_profile_creation=0x7f030004;
     }
     public static final class menu {
-        public static final int main=0x7f070000;
+        public static final int icreep_menu=0x7f080000;
+        public static final int main=0x7f080001;
+        public static final int main_menu=0x7f080002;
+        public static final int menu=0x7f080003;
+        public static final int profile_creation=0x7f080004;
     }
     public static final class string {
         public static final int action_settings=0x7f050002;
         public static final int app_name=0x7f050000;
+        public static final int content_descrip=0x7f050014;
+        public static final int edit_email=0x7f050009;
+        public static final int edit_name=0x7f050005;
+        public static final int edit_surname=0x7f050007;
+        public static final int email_label=0x7f050008;
         public static final int hello_world=0x7f050001;
+        public static final int icreep_main_menu_logo=0x7f05000f;
+        public static final int name_label=0x7f050004;
+        public static final int profile_picture_label=0x7f05000a;
+        public static final int save_user_details=0x7f05000b;
+        public static final int select_profile=0x7f050013;
+        public static final int select_proximity=0x7f050010;
+        public static final int select_reports=0x7f050012;
+        public static final int select_time_tracker=0x7f050011;
+        public static final int surname_label=0x7f050006;
+        public static final int title_activity_icreep_menu=0x7f05000e;
+        public static final int title_activity_main_menu=0x7f05000c;
+        public static final int title_activity_menu=0x7f05000d;
+        public static final int title_activity_profile_creation=0x7f050003;
     }
     public static final class style {
         /** 
@@ -61,10 +103,10 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f060000;
+        public static final int AppBaseTheme=0x7f070000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f060001;
+        public static final int AppTheme=0x7f070001;
     }
 }
