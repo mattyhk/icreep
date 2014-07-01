6bc025664c849daee1bec6ddf18ba940d5bc1c24
sdaweti <sdaweti@openboxsoftware.com>
2014-07-01 10:51:11 +0200

    working on database

--- src/com/example/icreep/iCreepHelper.java
+++ src/com/example/icreep/iCreepHelper.java
@@ -8,11 +8,27 @@ import android.database.sqlite.SQLiteOpenHelper;
 public class iCreepHelper extends SQLiteOpenHelper {
 	
 	//set db name and version
-	private final static String DATABASE_NAME = "icreepdatabase";
-	private final static int DATABASE_VERSION = 1;
+	private static final String DATABASE_NAME = "icreepdatabase";
+	//version changes every time the structure of the db changes
+	private static final int DATABASE_VERSION = 1;
 	
 	//define tables (1..6) in db
+	private final String DATABASE_NAME1 = "Beacon";
 	
+	
+	private final String DATABASE_NAME2 = "ZoneBeacon";
+	
+	
+	private final String DATABASE_NAME3 = "Zone";
+	
+	
+	private final String DATABASE_NAME4 = "Location";
+	
+	
+	private final String DATABASE_NAME5 = "User";
+	
+	
+	private final String DATABASE_NAME6 = "Reports";
 	
 
 	public iCreepHelper(Context context) {
@@ -22,14 +38,19 @@ public class iCreepHelper extends SQLiteOpenHelper {
 
 	@Override
 	public void onCreate(SQLiteDatabase db) {
-		// TODO Auto-generated method stub
-
+		// when creating the db for the first time (execuring the CREATE TABLE SQL) - this will be called
+		
+		//Beacon table
+		String create_query = "CREATE TABLE " + DATABASE_NAME1 + "( BEACON_ID INTEGER PRIMARY KEY AUTOMINCREMENT, Major LONG, Minor LONG);";
+		
+		String
+		
 	}
 
 	@Override
 	public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-		// TODO Auto-generated method stub
-
+		// u can backup db online, alter table, drop table and such here...
+		
 	}
 
 }
