d8f3f93a67b228a4616ee1ca1fe8b182c1c0d7a4
sdaweti <sdaweti@openboxsoftware.com>
2014-07-02 16:54:20 +0200

    can now validate user email

--- src/com/example/icreep/ProfileCreation.java
+++ src/com/example/icreep/ProfileCreation.java
@@ -11,17 +11,9 @@ public class ProfileCreation extends Activity {
 	
 	//views to extract user details from
 	EditText userName, userSurname, userPosition, userEmail;
+	
 	//find way to get photo
 	ImageView userPhoto;
-		EditText name = (EditText) findViewById(R.id.editText1_user_name);
-		EditText surname = (EditText) findViewById(R.id.editText2_user_surname);
-		EditText email = (EditText) findViewById(R.id.editText3_user_email);
-		EditText position = (EditText) findViewById(R.id.editText4_user_position);
-		
-		String sname = name.getText().toString(); 
-		String ssurname = surname.getText().toString(); 
-		String semail  = email.getText().toString();
-		String sposition  = position.getText().toString();
 		
 	String photo="";
 	
@@ -40,6 +32,7 @@ public class ProfileCreation extends Activity {
 					
 		//rename helper for db management
 		icreepHelper = new iCreepDatabaseAdapter(this);	
+		
 	}//onCreate method
 	
 	//upload image
@@ -48,8 +41,6 @@ public class ProfileCreation extends Activity {
     	//Intent intent = new Intent(this, ProfilePicture.class);
 		
     	photo = "";
-	}
-    	
 	}
 	
 	//listener to adddUser event - let's add new user to db	
@@ -66,15 +57,31 @@ public class ProfileCreation extends Activity {
 		String email = userEmail.getText().toString();
 		
 		//before entering user into DB - can send validation email first
-		//if validation email bounces than user not entered in DB else add to DB
+		//if validation email bounces than user not entered in DB else add to DB		
+		if(isValidEmail(email)){				
+			long id = icreepHelper.enterNewUser(name, surname, position, email, photo);	
 			
-		long id = icreepHelper.enterNewUser(name, surname, position, email, photo);	
-		
-		//check if insertion was successful
-		if(id<0){
-			Message.message(this, "User details saved");
-		}else{
-			Message.message(this, "User not details saved");
-		}		   	
-    }//saveDetails method	
+			//check if insertion was successful
+			if(id<0){
+				Message.message(this, "User details saved");
+			}else{
+				Message.message(this, "User details not  saved");
+			}		
+	    }
+		else{
+			Message.message(this,"invalid email address");
+		}		
+	}//saveDetails method
+	
+	//function to validate email addresses against an email Regular Expression
+    public boolean isValidEmail(String email){
+	    
+    	String emailRegex ="^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$";
+	    
+    	if(email.matches(emailRegex))
+	    {
+	        return true;
+	    }
+	    return false;
+    }
 }
