1115b77af346bb318db7a73f60bc14c454990acc
ncharlton <ncharlton@openboxsoftware.com>
2014-07-03 09:57:46 +0200

    Changes to icreepmenu and icreep manifest

--- src/icreep/app/IcreepMenu.java
+++ src/icreep/app/IcreepMenu.java
@@ -3,6 +3,7 @@ package icreep.app;
 import android.app.Activity;
 import android.app.ActionBar;
 import android.app.Fragment;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -10,43 +11,84 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.os.Build;
 import icreep.app.R;
 import icreep.app.Location.LocationActivity;
+import icreep.app.Report.reportActivity;
 
 public class IcreepMenu extends Activity {
 	
-	Button button;
+	Button proximity_button, time_tracker_button, reports_button, profile_button;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_icreep_menu);
+		addListenerOnButton();
 
 	}//onCreate
 	
-	public void userProximity(View view){
-    	 
-    	
-    }//selectProximity 
-
-	public void timeTracker(View view){
+	private void addListenerOnButton() {
 		
+		final Context context = this;
+		 
+		proximity_button = (Button) findViewById(R.id.button1);
+		time_tracker_button = (Button) findViewById(R.id.button2);
+		reports_button = (Button) findViewById(R.id.button3);
+		reports_button = (Button) findViewById(R.id.button4);		
+ 
+		proximity_button.setOnClickListener(new OnClickListener() {
+ 
+			@Override
+			public void onClick(View arg0) {
+ 
+			    Intent intent = new Intent(context, LocationActivity.class); //Correct proximity class needed here
+                            startActivity(intent);   
+ 
+			}//onClick
+ 
+		});
 		
-	}//timeTracker 
-	
-	public void userReports(View view){
+		time_tracker_button.setOnClickListener(new OnClickListener() {
+			 
+			@Override
+			public void onClick(View arg0) {
+ 
+			    Intent intent = new Intent(context, LocationActivity.class); //Correct time tracker class needed here
+                            startActivity(intent);   
+ 
+			}//onClick
+ 
+		});
 		
+		reports_button.setOnClickListener(new OnClickListener() {
+			 
+			@Override
+			public void onClick(View arg0) {
+ 
+			    Intent intent = new Intent(context, reportActivity.class); //Correct reports class needed here
+                            startActivity(intent);   
+ 
+			}//onClick
+ 
+		});
 		
-	}//userReports 
-	
-	public void userProfile(View view){
+		profile_button.setOnClickListener(new OnClickListener() {
+			 
+			@Override
+			public void onClick(View arg0) {
+ 
+			    Intent intent = new Intent(context, ProfileCreation.class); 
+                            startActivity(intent);   
+ 
+			}//onClick
+ 
+		});
 		
-		
-	}//userProfile 
+	}//addListenerOnButton
 	 
-
 ////////////////////////////////////////////////////////////////////////////////////////////////	
 	/*
 	@Override
@@ -96,4 +138,4 @@ public class IcreepMenu extends Activity {
 	}
 */
 	
-}
+}//IcreepMenu class
