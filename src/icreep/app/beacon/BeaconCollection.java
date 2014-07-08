9254aa7a3ab895a26f4d55f8e374ee00b61e9775
mkerr <mkerr@openboxsoftware.com>
2014-07-08 10:00:51 +0200

    Added nearest beacon functionality

--- src/icreep/app/beacon/BeaconCollection.java
+++ src/icreep/app/beacon/BeaconCollection.java
@@ -21,6 +21,9 @@ public class BeaconCollection {
 	
 	private final static int MAJOR = 3;
 	private final static int NUMBEACONS = 15;
+	private final static double DEFAULT_ACCURACY = 30;
+	
+	private int closestBeacon = -1;
 	
 	private final List<BeaconModel> myBeacons = new ArrayList<BeaconModel>();
 	
@@ -38,7 +41,10 @@ public class BeaconCollection {
 	 * Method to handle updating all of the MyBeacon objects, when receiving a collection
 	 * of iBeacons in range. 
 	 * Assumes that all iBeacons passed in have a minor key set within the maximum value of NUMBEACONS, and no two
-	 * iBeacons share the same minor key
+	 * iBeacons share the same minor key.
+	 * Modifies the closestBeacon field by first checking which of the beacons in range, if any,
+	 * is the closest. Sets the field to the beacon's index, otherwise sets it to -1 representing
+	 * Out of Office.
 	 */
 	public List<BeaconModel> processIBeacons(Collection<IBeacon> iBeacons) {
 			
@@ -58,46 +64,79 @@ public class BeaconCollection {
 		 * of iBeacons found, update the beacon.
 		 * If the beacon does not match any of the beacons found, update the beacon as a 
 		 * missing beacon.
+		 * Also, check which beacon is closest. If none of the beacons are in range, set 
+		 * the index to -1. Else, set the index to the index of the closest beacon.
 		 */
 		
 		int j = 0;
-		
-		if (iBeaconList.size() == 0) {
+		double tempAcc = DEFAULT_ACCURACY;
+		int closest = -1;
 			
-			for (int i = 0; i <= NUMBEACONS; i++) {
-				
-				BeaconModel mBeacon = myBeacons.get(i);
-				mBeacon.updateNotFound();
-				
-			}
-		}
-		
-		else {
+		for (int i = 0; i <= NUMBEACONS; i++) {
 			
-			for (int i = 0; i <= NUMBEACONS; i++) {
-				
-				BeaconModel mBeacon = myBeacons.get(i);
-				
-				if (j < iBeaconList.size()) {
-					if (mBeacon.getMinor() == iBeaconList.get(j).getMinor()) {
-						
-						mBeacon.updateBeaconModel(iBeaconList.get(j));
-						j++;
-						
-					}
-					
-					else {
-						mBeacon.updateNotFound();		
-					}
+			BeaconModel mBeacon = myBeacons.get(i);
+			
+			if (j < iBeaconList.size()) {
+				if (mBeacon.getMinor() == iBeaconList.get(j).getMinor()) {
+					mBeacon.updateBeaconModel(iBeaconList.get(j));
+					j++;	
 				}
 				
 				else {
-					mBeacon.updateNotFound();
+					mBeacon.updateNotFound();		
 				}
+			}
+			
+			else {
+				mBeacon.updateNotFound();
+			}
+			
+			double acc = mBeacon.getWeightedAccuracy();
+			if (acc < tempAcc) {
+				tempAcc = acc;
+				closest = i;
 			}
 		}
 		
-		
+		/*
+		 * Set the closest beacon index
+		 */
+		setClosestBeacon(closest);
+
 		return myBeacons;
+	}
+	
+	/**************************
+	 * 
+	 * Getters and Setters
+	 * 
+	 **************************/
+	
+	/**
+	 * Set the index of the closest iBeacon as determined by weighted average
+	 * If none of the beacons are in range, sets it to 'Outdoors', or -1
+	 * @param i - index of closest beacon
+	 */
+	private void setClosestBeacon(int i) {
+		
+		this.closestBeacon = i;
+	
+	}
+	
+	/**
+	 * Gets the Minor value of the closest beacon.
+	 * If there are no beacons in range, returns -1.
+	 * @return minor - the value of the minor of the closest beacon
+	 */
+	public int getClosestBeaconMinor() {
+		
+		if (this.closestBeacon == -1) {
+			return -1;
+		}
+		
+		else {
+			return myBeacons.get(closestBeacon).getMinor();	
+		}
+		
 	}
 }
