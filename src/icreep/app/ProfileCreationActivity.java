abd39a4ad0c1a71abc04216955428ffe061227bc
mkerr <mkerr@openboxsoftware.com>
2014-07-10 09:11:21 +0200

    Removed import

--- src/icreep/app/ProfileCreationActivity.java
+++ src/icreep/app/ProfileCreationActivity.java
@@ -18,7 +18,6 @@ import android.provider.MediaStore;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.WindowManager;
-import android.widget.Adapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
