d44235241be9058734d26072f885e9abb197d27d
ncharlton <ncharlton@openboxsoftware.com>
2014-07-10 12:10:15 +0200

    Removed onCreateOptionsMenu() from ReportActivity

--- src/icreep/app/report/ReportActivity.java
+++ src/icreep/app/report/ReportActivity.java
@@ -234,15 +234,6 @@ public class ReportActivity extends FragmentActivity
 	}
 
 	@Override
-	public boolean onCreateOptionsMenu(Menu menu)
-	{
-
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
-	}
-
-	@Override
 	public boolean onOptionsItemSelected(MenuItem item)
 	{
 		// Handle action bar item clicks here. The action bar will
